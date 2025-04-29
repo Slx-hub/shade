38707f43db9bf1986fc5698a6db8953abb884184
Slx-hub <schneja@tuta.io>
2025-04-29 13:00:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+## Shade
+This aims to control leds according to camera input for a personal project
---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install python3-pip python3-opencv libatlas-base-dev
+pip3 install flask flask-socketio eventlet
